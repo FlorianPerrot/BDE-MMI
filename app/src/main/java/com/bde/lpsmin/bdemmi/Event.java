5057af505801531f09b3414bc789be48e5718971
Florian Perrot <contact@florian-perrot.fr>
2014-10-07 14:29:53 +0200

    Methode

--- app/src/main/java/com/bde/lpsmin/bdemmi/Event.java
+++ app/src/main/java/com/bde/lpsmin/bdemmi/Event.java
@@ -1,6 +1,13 @@
 package com.bde.lpsmin.bdemmi;
 
+import java.util.ArrayList;
 import java.util.Date;
+
+import android.content.Context;
+
+import com.google.gson.JsonArray;
+import com.koushikdutta.async.future.FutureCallback;
+import com.koushikdutta.ion.Ion;
 
 /**
  * Created by lheido on 25/09/14.
@@ -9,7 +16,7 @@ public class Event extends Actu {
 
     private String lieu;
 
-    public Event(String title, String content, String imgUri, String lieu, Date date) {
+    public Event(String title, String content, String imgUri, String lieu, String date) {
         super(title, content, imgUri, date);
         this.lieu = lieu;
     }
@@ -20,5 +27,50 @@ public class Event extends Actu {
 
     public void setLieu(String lieu) {
         this.lieu = lieu;
+    }
+    static public ArrayList<Event> getActuListFromJson(boolean typeDEvent,Context context) {
+    	
+    	final ArrayList<Event> eventArray = new ArrayList<Event>();
+    	
+    	if(typeDEvent){
+			Ion.with(context)
+			.load("http://localhost/bde/json.php?json=1")
+			.asJsonArray()
+			.setCallback(new FutureCallback<JsonArray>(){
+				public void onCompleted(Exception e, JsonArray result) {
+			    	
+					for(int i=0; i<result.size() ;i++){
+						eventArray.add(new Event(
+								result.get(i).getAsJsonObject().get("title").getAsString(),
+								result.get(i).getAsJsonObject().get("conent").getAsString(),
+								result.get(i).getAsJsonObject().get("image_url").getAsString(),
+								result.get(i).getAsJsonObject().get("date").getAsString(),
+								result.get(i).getAsJsonObject().get("lieu").getAsString()
+						));
+					}
+				}
+			});
+    	}
+    	else{
+			Ion.with(context)
+			.load("http://localhost/bde/json.php?json=0")
+			.asJsonArray()
+			.setCallback(new FutureCallback<JsonArray>(){
+				public void onCompleted(Exception e, JsonArray result) {
+			    	
+					for(int i=0; i<result.size() ;i++){
+						eventArray.add(new Event(
+								result.get(i).getAsJsonObject().get("title").getAsString(),
+								result.get(i).getAsJsonObject().get("conent").getAsString(),
+								result.get(i).getAsJsonObject().get("image_url").getAsString(),
+								result.get(i).getAsJsonObject().get("date").getAsString(),
+								result.get(i).getAsJsonObject().get("lieu").getAsString()
+						));
+					}
+				}
+			});  		
+    	}
+		
+		return eventArray;
     }
 }
